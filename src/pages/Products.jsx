56bd0e8cc1cafe3b77b48ec35d36400c154141aa
dmychel <dmychel@tutanota.com>
2024-02-07 16:24:17 -0500

    added props

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
-const Products = ({ cart }) => {
+const Products = ({ cart, setCart }) => {
   console.log(cart);
   return <h1>Products</h1>;
 };
 
 Products.propTypes = {
   cart: PropTypes.array,
+  setCart: PropTypes.func,
 };
 
 export default Products;
