b25104d13dc27f83d866d3d941856ca319632a7e
dmychel <dmychel@tutanota.com>
2024-02-07 16:18:56 -0500

    passed props

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,11 @@
-const Cart = () => {
+import PropTypes from "prop-types";
+const Cart = ({ cart }) => {
+  console.log(cart);
   return <h1>Cart</h1>;
+};
+
+Cart.propTypes = {
+  cart: PropTypes.string,
 };
 
 export default Cart;
