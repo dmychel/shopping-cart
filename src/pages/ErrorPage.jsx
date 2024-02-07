d1d93c9546170e2ddcb63a9243defe70f9a7be74
dmychel <dmychel@tutanota.com>
2024-02-07 16:20:05 -0500

    removed useRouteError

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,16 @@
-import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  const error = useRouteError();
-  console.error(error);
-
   return (
     <div className="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
+
       <p>
-        <i>{error.statusText || error.message}</i>
+        Go to the
+        <Link to="/" style={{ textDecoration: "underline", color: "white" }}>
+          HomePage
+        </Link>
       </p>
     </div>
   );
