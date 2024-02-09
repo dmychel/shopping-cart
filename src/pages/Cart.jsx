387f92de9f624bad245ffbf10deeb50a9634f1d3
dmychel <dmychel@tutanota.com>
2024-02-08 20:31:30 -0500

    jsx for cart created

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,48 @@
 import PropTypes from "prop-types";
 const Cart = ({ cart }) => {
-  console.log(cart);
-  return <h1>Cart</h1>;
+  return (
+    <div className="cart">
+      <section className="cart-items">
+        {cart.map((item) => (
+          <div className="cart-card" key={item.id}>
+            <span>&#10005;</span>
+            <img src={item.image} alt="item-preview" />
+            <p>{item.title}</p>
+          </div>
+        ))}
+      </section>
+      <section className="checkout">
+        <div className="delivery">
+          <form>
+            <input type="email" placeholder="email address" />
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="text" placeholder="Street Address" />
+            <input type="text" placeholder="Zip Code" />
+            <input type="Country" placeholder="Country" />
+          </form>
+        </div>
+        <div className="summary">
+          <div>
+            <span>Subtotal</span>
+            <span>0</span>
+          </div>
+          <div>
+            <span>Delivery</span>
+            <span>0</span>
+          </div>
+          <div>
+            <span>Taxes</span>
+            <span>0</span>
+          </div>
+          <div>
+            <span>Total</span>
+            <span>0</span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 Cart.propTypes = {
