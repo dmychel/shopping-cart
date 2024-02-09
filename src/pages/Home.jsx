65b83b2c044105e3b5e9b2ee5ff7bc265ca5d17c
dmychel <dmychel@tutanota.com>
2024-02-08 21:40:17 -0500

    added jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 const Home = () => {
   return (
     <section className="home">
+      <h2>A safe haven for your shopping desires</h2>
       <div className="hero">
         <img src="/assets/imgs/hero.jpg" alt="Hero Image" />
       </div>
