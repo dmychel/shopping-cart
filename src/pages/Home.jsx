dedc361c72de836834edeef352be83704e767414
dmychel <dmychel@tutanota.com>
2024-02-08 21:40:55 -0500

    styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import { NavLink } from "react-router-dom";
 const Home = () => {
   return (
     <section className="home">
-      <h2>A safe haven for your shopping desires</h2>
+      <h2 style={{ fontSize: "1.5rem", textAlign: "center" }}>
+        A safe haven for your shopping desires
+      </h2>
       <div className="hero">
         <img src="/assets/imgs/hero.jpg" alt="Hero Image" />
       </div>
