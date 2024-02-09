64e2bbd1062819280bed9be2788ec98a52ef0c67
dmychel <dmychel@tutanota.com>
2024-02-08 19:51:53 -0500

    added hero div and image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import { NavLink } from "react-router-dom";
 const Home = () => {
   return (
     <section className="home">
+      <div className="hero">
+        <img src="/assets/imgs/hero.jpg" alt="Hero Image" />
+      </div>
       <NavLink to="products">Shop Now</NavLink>
     </section>
   );
